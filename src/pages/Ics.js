50645e3caa854d53d5d293dff9eccc2b945da59c
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2023-10-26 18:17:12 -0300

    feat: implemented

--- src/pages/Ics.js
+++ src/pages/Ics.js
@@ -2,17 +2,66 @@ import { useParams } from "react-router-dom";
 import { styled } from "styled-components";
 import { Header, Container, Line } from "./Initial";
 import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function IcsPage() { 
-    const [ process, setProcess ] = useState([]);
-    const [ toggle, setToggle ] = useState(false);
+    //const [ process, setProcess ] = useState([]);
     const { name } = useParams();
 
+    //DESCOMENTAR DEPOIS DE CONECTAR O BANCO
     // useEffect(async() => { 
-    //     //consumir os dados do back
-    //     // const promise = await function ...
-    //     // setProcess(promise.data); 
+    //     try {
+    //         const promise = await axios.get(`link do banco/${name}`);
+    //         setProcess(promise.data);
+    //     } catch (error) {
+    //         console.log(error);
+    //     }
+         
     // },[]);
+
+    async function changeState(id,status) { 
+        if(status === "Funcionando") { 
+            //DESCOMENTAR DEPOIS DE CONECTAR O BANCO
+            const info = { status: "Parado" };
+            //await axios.put(`link do banco/${id}`,info);
+            console.log("Trocou o status");
+        } else {
+            //DESCOMENTAR DEPOIS DE CONECTAR O BANCO
+            const info = { status: "Funcionando" };
+            //await axios.put(`link do banco/${id}`,info);
+            console.log("Trocou o status");
+        }
+
+        //SE NAO ATUALIZAR O ESTADO ASSIM QUE TROCAR DESCOMENTAR A LINHA DEBAIXO, CASO CONTRATRIO SO EXCLUIR A LINHA ABAIXO
+        //window.location.reload();
+    }
+
+    const process = [ 
+        {
+            id: 1,
+            nome: "Biblioteca",
+            ic: "VMAWS0101", 
+            criticidade: 2, 
+            responsavel: "Engenheiro", 
+            status: "Funcionando"
+        }, 
+        {
+            id: 2,
+            nome: "Biblioteca",
+            ic: "VMAWS0101", 
+            criticidade: 2, 
+            responsavel: "Engenheiro", 
+            status: "Funcionando"
+        }, 
+        {
+            id: 3,
+            nome: "Gerenciamento Estudantil",
+            ic: "VMAWS0101", 
+            criticidade: 2, 
+            responsavel: "Engenheiro", 
+            status: "Parado"
+        }
+    ];
 
     return(
         <>
@@ -30,14 +79,17 @@ export default function IcsPage() {
                         <p>Status</p>
                     </MainTittles>
                     <Line>.</Line>
-                    <RenderProcess status="Funcionando">
-                        <span>Biblioteca</span>
-                        <span>AWSVM0102</span>
-                        <State status="Funcionando">
-                            <span>Parado</span>
-                            <button onClick={() => setToggle(!toggle)}></button>
-                        </State>
-                    </RenderProcess>
+
+                    {process.map(proces => (
+                    <RenderProcess status={proces.status}>
+                        <span id="name">{proces.nome}</span>
+                        <span>{proces.ic}</span>
+                    <State status={proces.status}>
+                        <span>{proces.status}</span>
+                        <button onClick={() => changeState(proces.id,proces.status)}>Troca</button>
+                    </State>
+                     </RenderProcess>
+                    ))}
                 </Box>
             </Processos>
         </Container>
@@ -85,18 +137,30 @@ const RenderProcess = styled.div`
         color: ${props => props.status === "Parado" ? "red" : "black"};
         font-size: 20px;
     }
+
+    span#name { 
+        width: 140px;
+        word-break: break-all;
+        text-align: center;
+    }
 `
 const State = styled.div`
     display: flex; 
     align-items: center;
 
     button { 
-        width: 20px;
-        height: 20px;
+        width: 40px;
+        height: 35px;
         border-radius: 50%;
         border: none;
         background-color: ${props => props.status === "Funcionando" ? "green" : "red"};
         margin-left: 5px;  
+        color: white;
+        font-size: 10px;
+        display: flex; 
+        justify-content: center;
+        align-items: center;
+        font-weight: bold;
 
         &:hover{ 
             cursor: pointer; 
